6a39ed5868c0d77e8a25cafb093c894dca96d473
muhammadasadbastomi <tomycules@gmail.com>
2020-05-31 02:07:40 +0800

    crud function modul controller

--- app/Http/Controllers/modulController.php
+++ app/Http/Controllers/modulController.php
@@ -2,22 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Modul;
 use Illuminate\Http\Request;
 
 class modulController extends Controller
 {
     public function index()
     {
-        return view('admin.modul.index');
+        $data = Modul::orderBy('id', 'desc')->get();
+        return view('admin.modul.index', compact('data'));
     }
 
-    public function show()
+    public function preview($uuid)
     {
-        return view('admin.modul.show');
+        $data = modul::where('uuid', $uuid)->first();
+        $pathToFile = base_path() . '/public/modul/' . $data->file;
+
+        return response()->file($pathToFile);
     }
 
-    public function edit()
+    public function store(Request $request)
     {
-        return view('admin.modul.edit');
+        $data = new modul;
+        $data->kode_modul = $request->kode_modul;
+        $data->judul = $request->judul;
+        if ($request->file != null) {
+            $img = $request->file('file');
+            $fileExt = $img->getClientOriginalExtension();
+            $fileName = $request->kode_modul;
+            $file = $fileName . '.' . $fileExt;
+            $img->move('modul', $file);
+            $data->file = $file;
+        }
+
+        $data->save();
+
+        return redirect()->route('modulIndex')->with('success', 'Data Berhasil Disimpan');
+    }
+
+    public function edit($uuid)
+    {
+        $data = modul::where('uuid', $uuid)->first();
+        return view('admin.modul.edit', compact('data'));
+    }
+
+    public function update(Request $request, $uuid)
+    {
+        $data = modul::where('uuid', $uuid)->first();
+        $data->kode_modul = $request->kode_modul;
+        $data->judul = $request->judul;
+        if ($request->file != null) {
+            $img = $request->file('file');
+            $fileExt = $img->getClientOriginalExtension();
+            $fileName = $request->kode_modul;
+            $file = $fileName . '.' . $fileExt;
+            $img->move('modul', $file);
+            $data->file = $file;
+        } else {
+            $data->file = $data->file;
+        }
+
+        $data->update();
+
+        return redirect()->route('modulIndex')->with('success', 'Data Berhasil Diubah');
+    }
+
+    public function destroy($uuid)
+    {
+        $modul = modul::where('uuid', $uuid)->first()->delete();
+
+        return redirect()->route('modulIndex')->with('success', 'Berhasil menghapus data');
+
     }
 }
