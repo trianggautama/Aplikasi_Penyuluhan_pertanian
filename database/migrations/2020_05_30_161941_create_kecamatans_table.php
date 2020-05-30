cb7c5b13da03f28d52121e08f1a3be5c83b109e2
muhammadasadbastomi <tomycules@gmail.com>
2020-05-31 00:46:15 +0800

    setup migration users table

---
+++ database/migrations/2020_05_30_161941_create_kecamatans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKecamatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kecamatans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('uuid', 36);
+            $table->string('kecamatan', 50);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kecamatans');
+    }
+}
