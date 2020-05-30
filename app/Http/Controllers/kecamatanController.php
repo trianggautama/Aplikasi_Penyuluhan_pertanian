9b6fc7292be1f2c07e9b1cad8a14cbb0facb6238
muhammadasadbastomi <tomycules@gmail.com>
2020-05-31 00:45:23 +0800

    create crud method kecamatan controller

--- app/Http/Controllers/kecamatanController.php
+++ app/Http/Controllers/kecamatanController.php
@@ -2,17 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Kecamatan;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class kecamatanController extends Controller
 {
     public function index()
     {
-        return view('admin.kecamatan.index');
+        $data = User::where('role', 1)->orderBy('id', 'desc')->get();
+        return view('admin.kecamatan.index', compact('data'));
     }
 
-    public function edit()
+    public function store(Request $request)
     {
-        return view('admin.kecamatan.edit');
+        $data = new User;
+        $data->nama = $request->nama;
+        $data->username = $request->username;
+        $data->password = Hash::make($request->password);
+        if ($request->foto != null) {
+            $img = $request->file('foto');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $request->nama;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('images/user', $foto);
+            $data->foto = $foto;
+        } else {
+            $data->foto = 'default.jpg';
+        }
+        $data->role = 1;
+
+        $data->save();
+
+        $kecamatan = new Kecamatan;
+        $kecamatan->user_id = $data->id;
+        $kecamatan->kecamatan = $request->kecamatan;
+
+        $kecamatan->save();
+
+        return redirect()->route('kecamatanIndex')->with('success', 'Data Berhasil Disimpan');
+    }
+
+    public function edit($uuid)
+    {
+        $data = User::where('uuid', $uuid)->first();
+        return view('admin.kecamatan.edit', compact('data'));
+    }
+
+    public function update(Request $request, $uuid)
+    {
+        $data = User::where('uuid', $uuid)->first();
+        $data->nama = $request->nama;
+        $data->username = $request->username;
+        $data->password = Hash::make($request->password);
+        if ($request->foto != null) {
+            $img = $request->file('foto');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $request->nama;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('images/user', $foto);
+            $data->foto = $foto;
+        } else {
+            $data->foto = $data->foto;
+        }
+
+        $data->update();
+
+        $kecamatan = Kecamatan::where('id', $data->kecamatan->id)->first();
+        $kecamatan->user_id = $data->id;
+        $kecamatan->kecamatan = $request->kecamatan;
+
+        $kecamatan->update();
+
+        return redirect()->route('kecamatanIndex')->with('success', 'Data Berhasil Diubah');
+    }
+
+    public function destroy($uuid)
+    {
+        $user = User::where('uuid', $uuid)->first()->delete();
+
+        return redirect()->route('kecamatanIndex')->with('success', 'Berhasil menghapus data');
+
     }
 }
