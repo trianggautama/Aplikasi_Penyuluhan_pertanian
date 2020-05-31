e5f4e19dde042453c3e5ab731e418e007e5beb52
muhammadasadbastomi <tomycules@gmail.com>
2020-05-31 21:48:20 +0800

    add route middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'kecamatan' => \App\Http\Middleware\Kecamatan::class,
+        'admin' => \App\Http\Middleware\Admin::class,
     ];
 }
